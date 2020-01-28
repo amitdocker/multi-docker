3410fa09aa07b07339007234f02637720623f054
amitdocker <fullstack.amit@gmail.com>
2020-01-28 17:32:44 +0530

    app.js file changed

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Fib Calculator</h1>
             <Link to="/">Home</Link>
-            <Link to="/otherpage">About Us workdir area</Link>
+            <Link to="/otherpage">AboutUs</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
